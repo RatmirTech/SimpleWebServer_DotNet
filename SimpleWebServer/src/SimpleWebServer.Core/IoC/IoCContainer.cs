0db94191c391730f71844329eee7320c3e14e187
Ratmir Ishgulov <ishgulov.ratmir@gmail.com>
2024-07-30 02:48:54 +0300

    #4 Добавил Ioc базовый с Register и Resolve. Храню зарегистрированные объекты в Dictionary<Type, Type>. Нужно передалть под концепцию Singleton, Scoped, Transient.

--- SimpleWebServer/src/SimpleWebServer.Core/IoC/IoCContainer.cs
+++ SimpleWebServer/src/SimpleWebServer.Core/IoC/IoCContainer.cs
@@ -2,5 +2,16 @@
 {
     public class IoCContainer
     {
+        private static readonly Dictionary<Type, Type> _registrations = new();
+
+        private static void Register<Tkey, TConcrete>()
+        {
+            _registrations[typeof(TConcrete)] = typeof(TConcrete);
+        }
+
+        private static dynamic Resolve<Tkey>()
+        {
+            return Activator.CreateInstance(_registrations[typeof(Tkey)]);
+        }
     }
 }
