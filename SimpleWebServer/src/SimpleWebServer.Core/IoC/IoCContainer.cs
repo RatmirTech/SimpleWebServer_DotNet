7ec7243b20410cc48df43432af3dc06e0e09e98b
Ratmir Ishgulov <ishgulov.ratmir@gmail.com>
2024-07-30 03:07:28 +0300

    #4 fix _registrations[typeof(Tkey)] = typeof(TConcrete), ранее Tkey = Tkey... 2 different Resolve: Resolve<Tkey> and Resolve(Type key)
    Add singleton, scoped and transient types

--- SimpleWebServer/src/SimpleWebServer.Core/IoC/IoCContainer.cs
+++ SimpleWebServer/src/SimpleWebServer.Core/IoC/IoCContainer.cs
@@ -2,16 +2,56 @@
 {
     public class IoCContainer
     {
-        private static readonly Dictionary<Type, Type> _registrations = new();
-
-        private static void Register<Tkey, TConcrete>()
+        private enum Lifetime
         {
-            _registrations[typeof(TConcrete)] = typeof(TConcrete);
+            Singleton,
+            Scoped,
+            Transient
         }
 
-        private static dynamic Resolve<Tkey>()
+        private class Registration
         {
-            return Activator.CreateInstance(_registrations[typeof(Tkey)]);
+            public Type ConcreteType { get; }
+            public Lifetime Lifetime { get; }
+
+            // for Singleton & Scoped
+            public object Instance { get; set; }
+
+            public Registration(Type concreteType, Lifetime lifetime)
+            {
+                ConcreteType = concreteType;
+                Lifetime = lifetime;
+            }
+        }
+
+        private static readonly Dictionary<Type, Registration> _registrations = new();
+        private static Dictionary<Type, object> _scopedInstances;
+
+        public static void RegisterSingleton<Tkey, TConcrete>()
+        {
+            _registrations[typeof(Tkey)] = new Registration(typeof(TConcrete), Lifetime.Singleton);
+        }
+
+        public static void RegisterScoped<Tkey, TConcrete>()
+        {
+            _registrations[typeof(Tkey)] = new Registration(typeof(TConcrete), Lifetime.Scoped);
+        }
+
+        public static void RegisterTransient<Tkey, TConcrete>()
+        {
+            _registrations[typeof(Tkey)] = new Registration(typeof(TConcrete), Lifetime.Transient);
+        }
+
+        public static Tkey Resolve<Tkey>()
+        {
+            // only for customers, don't using it in system
+            return (Tkey)Resolve(typeof(Tkey));
+        }
+
+        private static dynamic Resolve(Type type)
+        {
+            //TODO: Сделать рабочий Resolve
+            return null;
         }
     }
 }
